2ac001ddb1249d52336781a740fcb47c75b4bb4f
laotu <2427796523@qq.com>
2025-06-10 18:24:48 +0800

    add GeneratePresetSetting method to allow custom component preset

---
+++ src/Secyud.Secits.Blazor/Components/Basics/ScBusinessBase.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Secyud.Secits.Blazor.Components;
+
+public abstract partial class ScBusinessBase
+{
+    private readonly ScSettingMaster _settingMaster;
+
+    protected ScBusinessBase()
+    {
+        _settingMaster = new ScSettingMaster(this);
+    }
+
+    public void RefreshUi()
+    {
+        StateHasChanged();
+    }
+
+    public Task RefreshUiAsync()
+    {
+        return InvokeAsync(StateHasChanged);
+    }
+}
