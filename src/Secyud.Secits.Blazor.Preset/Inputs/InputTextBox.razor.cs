ceed08e01383cd895c47738eba05e66d1ad037b8
laotu <2427796523@qq.com>
2025-06-10 18:32:15 +0800

    add preset project

---
+++ src/Secyud.Secits.Blazor.Preset/Inputs/InputTextBox.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Secyud.Secits.Blazor.Preset;
+
+public partial class InputTextBox
+{
+    [Parameter]
+    public bool SubmitOnInput { get; set; }
+}
